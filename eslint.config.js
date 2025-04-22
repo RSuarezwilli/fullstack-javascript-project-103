5e5317a9229f01f72777b4364f7c7834b3ece781
root <root@DESKTOP-A93JRRE.>
2025-04-21 19:04:38 -0500

    mis cambios

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "single"],
+        indent: ["error", 2],
+        "no-unused-vars": ["warn"],
+        "no-console": "off"
+      },
+    },
+  ];
+  
