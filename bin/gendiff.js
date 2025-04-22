9779b0a86089b5a20a950dca78b38c900dabd2bf
root <root@DESKTOP-A93JRRE.>
2025-04-22 15:47:41 -0500

    mis cambios

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 import {program} from 'commander';
 import gendiff from '../src/utils/diff.js';
 
+
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
