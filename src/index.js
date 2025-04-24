1a677b625b60c5563930d5ac63c1ceb4c7832044
root <root@DESKTOP-A93JRRE.>
2025-04-24 12:55:03 -0500

    mis cambios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-// src/utils/index.js
-import gendiff from './gendiff.js';
 
-const file1 = './__fixtures__/file1.json';
-const file2 = './__fixtures__/file2.json';
+export default function genDiff(path1, path2, formatType = 'stylish') {
 
-export default gendiff;
-
-const result = gendiff(file1, file2);
-console.log(result);
+}
