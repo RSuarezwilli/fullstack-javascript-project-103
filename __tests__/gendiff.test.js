654690bf1a0d03cbfc63bbe2c9fc8a9892ce8a58
root <root@DESKTOP-A93JRRE.>
2025-04-28 15:18:35 -0500

    mis cambios

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,15 +5,11 @@ import gendiff from '../src/index.js'; // <-- Import correcto (asegúrate de la 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
 test('gendiff basic test', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
   const expected = readFile('__fixtures__/expected.txt');
-
   expect(gendiff(file1, file2)).toEqual(expected.trim());
 });
-
--- src/index.js
+++ src/index.js
@@ -4,25 +4,20 @@ import buildDiff from './utils/diff.js';
 import stylish from './formatters/stylish.js';
 import parse from './parsers/parsers.js';
 
-
 const getFileContent = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath);
   return readFileSync(absolutePath, 'utf-8');
 };
 
 const getFileFormat = (filepath) => path.extname(filepath).slice(1);
-
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const content1 = getFileContent(filepath1);
   const content2 = getFileContent(filepath2);
-
   const obj1 = parse(content1, getFileFormat(filepath1));
   const obj2 = parse(content2, getFileFormat(filepath2));
 
   const diff = buildDiff(obj1, obj2);
-
   if (format === 'stylish') return stylish(diff);
   throw new Error(`Unsupported format: ${format}`);
 };
-
 export default genDiff;
