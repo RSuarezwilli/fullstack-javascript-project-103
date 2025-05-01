426e35c041f2b890fc8c1637925e5bc187ef858c
root <root@DESKTOP-A93JRRE.>
2025-04-30 20:39:17 -0500

    mis cambios

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,11 +8,15 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
-
+// const normalize = (str) => str.replace(/\r\n/g, '\n').trim();
 test('gendiff basic test', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const expected = readFile('stylish.result.txt');
-  console.log('Expected:', expected);
-  expect(gendiff(filepath1, filepath2)).toEqual(expected);
+  const received = gendiff(filepath1, filepath2);
+  // console.log('Expected:\n', expected);
+  // console.log('Received:\n', received);
+  // console.log('Expected:', expected);
+  // expect(normalize(received)).toEqual(normalize(expected));
+  expect(received).toEqual(expected);
 });
