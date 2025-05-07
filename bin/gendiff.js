743540916fa1849f939b0c5e8c82e84b981ab58e
root <root@DESKTOP-A93JRRE.>
2025-05-07 15:29:00 -0500

    mis cambios

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,17 @@
+#!/usr/bin/env node
+
 import { program } from 'commander';
-import gendiff from '../index.js';
+import gendiff from '../src/index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .argument('<filepath1.json>', 'the path to the first file')
-  .argument('<filepath2.json>', 'the path to the second file')
   .action((filepath1, filepath2, options) => {
-    const diff = gendiff(filepath1, filepath2, options.format);
-    console.log(diff);
-    console.log('filepath1:', filepath1);
-    console.log('filepath2:', filepath2);
-    gendiff(filepath1, filepath2);
+    const result = gendiff(filepath1, filepath2, options.format);
+    console.log(result);
   });
+
 program.parse(process.argv);
